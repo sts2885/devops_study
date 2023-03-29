02872ad2784d9495cdbee81f4cd6d12ea701f338
Son Taeseon <escs34@gmail.com>
2023-03-29 23:52:09 +0900

    security group added. next time fix user_data to run httpd correctly

--- 2-3 보안그룹추가와 immutable infrastructure/main.tf
+++ 2-3 보안그룹추가와 immutable infrastructure/main.tf
@@ -4,9 +4,6 @@ resource "aws_instance" "example" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.instance.id]
 
-    #이거 아마 옛날 코드라 안돌아가는 거 같은데
-    #문제는 key가 안주어져서 들어가 확인이 불가함.
-    #내일 다시 하자
     #EOF 는 heredoc syntax
     user_data = <<-EOF
                 #!/bin/bash
