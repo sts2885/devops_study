c530d7d9756d153788a8d5469a9a1ced8ad9a2be
Cloud User <rocky@monitoring-server.novalocal>
2023-07-25 00:20:27 +0900

    spark pipeline making completed and tensorflow pipeline making completed both of them processed by resourceOP

--- Kubernetes_study_folder/[EKS] 쿠버네티스 클러스터 생성/bootstrap_ctrl_and_kf.tf
+++ Kubernetes_study_folder/[EKS] 쿠버네티스 클러스터 생성/bootstrap_ctrl_and_kf.tf
@@ -25,7 +25,12 @@ resource "null_resource" "bootstrap_ctrl_and_kf" {
             "sleep 1",
             "nohup bash /home/ubuntu/port_forward_minio.sh 0<&- &> /home/ubuntu/kubeflow2.log &",
             "sleep 1",
-            "sudo apt install -y net-tools"
+            "sudo apt install -y net-tools",
+            "helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator",
+
+            "helm install my-release spark-operator/spark-operator --namespace kubeflow-user-example-com --create-namespace",
+
+            "kubectl apply -f /home/ubuntu/my_spark_service.yaml"
         ]
    }
 }
