1996312bdfc243559a5c3260448d825e39f0d36a
sts <sts0623@gmail.com>
2023-12-07 21:24:38 +0900

    test push for new jenkins at eks

--- Jenkins_study_folder/helloworld/src/main/java/com/HelloWorld/App.java
+++ Jenkins_study_folder/helloworld/src/main/java/com/HelloWorld/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println("test push again");
         System.out.println("test push for jenkins");
         System.out.println("Merge를 위한 push 테스트를 합니다.");
+        System.out.println("Need one more push for new jenkins at EKS.");
     }
 }
